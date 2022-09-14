5f682751ee9ecf0cff9c06280337bf07a71edd50
Katabe <ladykipoka@gmail.com>
2022-09-14 14:08:27 +0200

    Last commit

--- my_car_site/car/admin.py
+++ my_car_site/car/admin.py
@@ -1,3 +1,12 @@
+import imp
 from django.contrib import admin
-
+from car.models import Car
 # Register your models here.
+class MyCarAdmin(admin.ModelAdmin):
+    fieldsets=[
+        ('YEAR INFORMATION', {'fields':['year']}),
+        ('CAR INFORMATION', {'fields':['brand']})
+    ]
+
+
+admin.site.register(Car,MyCarAdmin)
